e70e2c148f3930cd833bc072f5b45d6d71a5eb9b
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2025-02-21 11:16:55 +0530

    S3 upload

--- s3_upload_media_package.sh
+++ s3_upload_media_package.sh
@@ -4,20 +4,20 @@ while [ true ]
 do
 
 
-sourcepath="/home/apalya"
+sourcepath="/home/user"
 
         cd $sourcepath
-        basename=`head -1 /home/apalya/upload_folder_for_mediapackage_demo.txt`
+        basename=`head -1 /home/user/upload_folder_for_mediapackage_demo.txt`
         if [ "$basename" != "" ]
         then
-        cd /home/apalya/
-        sed -i "/$basename\b/d" /home/apalya/upload_folder_for_mediapackage_demo.txt
-        logupload="/home/apalya/logs/upload_"$basename".log"
+        cd /home/user/
+        sed -i "/$basename\b/d" /home/user/upload_folder_for_mediapackage_demo.txt
+        logupload="/home/user/logs/upload_"$basename".log"
         path=/mnt/storage/mediapackage/$basename/
         echo "$basename exists" >> $logupload
                 echo "found $basename `date`" >> $logupload
                 echo "starting uploading `date`" >> $logupload
-                cd /home/apalya
+                cd /home/user/
                 /usr/local/bin/aws s3 sync  $path   s3://bucket-name/$basename/ >> $logupload
                 echo "$basename `date`" >> $sourcepath/logs/upload_comp_idmp.txt
                 echo "completed uploading `date`" >> $logupload
